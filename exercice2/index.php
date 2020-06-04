830cf3e2235b630a668bc168be42595dab38db90
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-04 14:49:35 +0200

    exo 1 et 2 fini

--- exercice2/index.php
+++ exercice2/index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo1</title>
+    <title>exo2</title>
 </head>
 
 <body>
-    <h1>Exercice 1</h1>
+    <h1>Exercice 2</h1>
 
     <p>Créer une variable <b>isEasy</b> de type booléan et l'initialiser avec une valeur. <br>
         Afficher <i>C'est facile !!</i> si c'est vrai. Dans le cas contraire afficher <i>C'est difficile !!!</i>. </p>
@@ -17,7 +17,7 @@
 
 
     <?php
-$isEasy = true;
+$isEasy = false;
 
 if ($isEasy == true){
     echo "<i>C'est facile !!</i><br>";
@@ -32,6 +32,7 @@ switch ($isEasy) {
     case false:
       echo "<i>C'est difficile !!!</i><br>";
       break;
+    }
 ?>
 
 </body>
